afcd46ae474dc1e814df4d23b517a498864de905
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-07-23 02:56:51 +1000

    style profile page

--- app/controllers/market_controller.rb
+++ app/controllers/market_controller.rb
@@ -1,9 +1,9 @@
 class MarketController < ApplicationController
   #home controller for static homepage of marketplace
   # before_action :authenticate_user!
+  before_action :hold_profile
   def home
-    @profile = Profile.where(user: current_user).first
-    @user = current_user
+
   end
 
   def category
@@ -13,9 +13,18 @@ class MarketController < ApplicationController
 
   def about
 
+
   end
   def contact
+    
 
+  end
+
+  private
+
+  def hold_profile
+    @profile = Profile.where(user: current_user).first
+    @user = current_user
   end
 
 
