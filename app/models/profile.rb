810a3c3afd1837b5bdcd2703c8519d54de3bfcad
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-07-27 12:17:47 +1000

    fixing bug : image upload optional to see community for now. will add more specifications later

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,8 +5,9 @@ class Profile < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
-  validates :suburb, presence: true
+  validates :suburb, presence: false
   validates :postcode, presence: true
-  validates :mobile_number, presence: true
-  validates :picture, presence: true
+  validates :mobile_number, presence: false
+  validates :picture, presence: false
+  validates :abn, presence: true
 end
