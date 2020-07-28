000765dcddcb328dad6ec6b3e7a1d05a75a27fdc
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-07-29 01:41:37 +1000

    fix bug on cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 class Cart < ApplicationRecord
   belongs_to :product
   belongs_to :buyer, class_name: 'User', optional: false
-  validates :price, presence: true
-  validates :image, presence: true
-  validates :title, presence: true
+  validates :product, presence: true
+  validates :buyer, presence: true
+
 end
