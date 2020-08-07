be466d8c030189dcbce08c22098ada99cfd8734d
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-08-08 00:10:11 +1000

    add comments on transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,14 @@
 
 class TransactionsController< ApplicationController
 
+  # Fetching the stripe api key form credentials
+  # querying a cart to process the payment when current user is the buyer
+  # sum ups the total cost of cart products
+  # creating a new customer and charge in Stripe to process the bill
+  # creating new order with the details of buyers
+  # saving the order items for showing those in order list
+  # after saving the items, destroying the cart
+  # if any error occurs (like item is not saved), redirecting user to my cart page
   def create
     Stripe.api_key = Rails.application.credentials.dig(:stripe, :development).dig(:secret_key)
     carts = Cart.where(buyer: current_user)
